8b312c1df8c950f804b2a7b2a5c9ea4648217594
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-05-14 12:34:23 -0700

    IndexOf

--- Ejemplos/Program.cs
+++ Ejemplos/Program.cs
@@ -16,9 +16,9 @@
 //Console.WriteLine(numeroConCeros);
 
 
-string nombre = "Jose";
-string apellido = "Rodriguez";
-int edad = 20;
+//string nombre = "Jose";
+//string apellido = "Rodriguez";
+//int edad = 20;
 
 //string NombreyEdad = nombre + "|" + apellido + "|" + edad;
 //Console.WriteLine(NombreyEdad);
@@ -27,7 +27,23 @@ int edad = 20;
 //Console.WriteLine(NombreyEdad);
 
 //El mejor metodo
-string NombreyEdad = $"{nombre}|{apellido}|{edad}";
-Console.WriteLine(NombreyEdad);
+//string NombreyEdad = $"{nombre}|{apellido}|{edad}";
+//Console.WriteLine(NombreyEdad);
 
 //Investigar concat
+
+//int numeroInt = Convert.ToInt32(numeroString);
+
+
+//int numeroInt = int.Parse(numeroString);
+//Console.WriteLine(numeroInt);
+
+
+//string numeroString = "5";
+//int numeroInt = 0;
+//bool pudo = int.TryParse(numeroString, out numeroInt);
+//Console.WriteLine(pudo);
+
+
+string cadena = "Hola Mundo";
+Console.WriteLine(cadena.IndexOf(" "));
