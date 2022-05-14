49eb5276c6e7123277fb2a402a125376ac87683f
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-05-14 11:39:30 -0700

    IsNullOrWhite, PadLeft

--- Ejemplos/Program.cs
+++ Ejemplos/Program.cs
@@ -1,4 +1,16 @@
-﻿string nombre = " Jose Carlos ";
-//Console.WriteLine(nombre.Trim());
+﻿//string nombre = " ";
+//Console.WriteLine(nombre.Length);
 
+//if (string.IsNullOrWhiteSpace (nombre))
+//{
+//    Console.WriteLine("string vacio");
+//}
+//else
+//{
+//    Console.WriteLine(nombre);
+//}
 
+int numero = 9;
+string numeroConCeros = "99"; 
+numeroConCeros = numeroConCeros.PadLeft( 3 , '$');
+Console.WriteLine(numeroConCeros);
