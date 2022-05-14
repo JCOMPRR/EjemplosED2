e67ece2704dae6427c50d8036f034d151f768ba5
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-05-14 11:52:52 -0700

    Trabajamos con Strings

--- Ejemplos/Program.cs
+++ Ejemplos/Program.cs
@@ -10,7 +10,24 @@
 //    Console.WriteLine(nombre);
 //}
 
-int numero = 9;
-string numeroConCeros = "99"; 
-numeroConCeros = numeroConCeros.PadLeft( 3 , '$');
-Console.WriteLine(numeroConCeros);
+//int numero = 9;
+//string numeroConCeros = "99"; 
+//numeroConCeros = numeroConCeros.PadLeft( 3 , '$');
+//Console.WriteLine(numeroConCeros);
+
+
+string nombre = "Jose";
+string apellido = "Rodriguez";
+int edad = 20;
+
+//string NombreyEdad = nombre + "|" + apellido + "|" + edad;
+//Console.WriteLine(NombreyEdad);
+
+//string NombreyEdad = string.Format("{0}|{1}|{2}", nombre, apellido, edad);
+//Console.WriteLine(NombreyEdad);
+
+//El mejor metodo
+string NombreyEdad = $"{nombre}|{apellido}|{edad}";
+Console.WriteLine(NombreyEdad);
+
+//Investigar concat
